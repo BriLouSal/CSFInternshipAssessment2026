d904ae1a048b365a784c9ff099ff352f975d7669
Brian Louis <brianlouis.salinas116@gmail.com>
2026-05-14 18:46:37 -0600

    Error Handling - Added Defensive measures that prevents the violation of unique tag_number - Return 400 respones instead of SQL constraint error - This will improve API stability and reliability

--- track2-fullstack/app/backend/routes/animals.js
+++ track2-fullstack/app/backend/routes/animals.js
@@ -43,29 +43,42 @@ router.post('/', (req, res) => {
   // Full atomicity is later enforced using database transactions, and this process will help the program
   // reduce the risk of inconsistent database state.
 
-  const result = db
-    .prepare(
-      'INSERT INTO animals (name, tag_number, breed, date_of_birth, paddock_id) VALUES (?, ?, ?, ?, ?)'
-    )
-    .run(
-      name,
-      tag_number,
-      breed ?? null,
-      date_of_birth ?? null,
-      paddock_id ?? null
-    )
-  // Now we replace the if-statement, we want to check if it's not null and  not undefined. Now for this let's handle the cases where the paddock_id has a better if-statement that handle
+  // We'll add error handling by preventing duplicate tag_number.
+  try {
+    const result = db
+      .prepare(
+        'INSERT INTO animals (name, tag_number, breed, date_of_birth, paddock_id) VALUES (?, ?, ?, ?, ?)'
+      )
+      .run(
+        name,
+        tag_number,
+        breed ?? null,
+        date_of_birth ?? null,
+        paddock_id ?? null
+      )
+    // Now we replace the if-statement, we want to check if it's not null and  not undefined. Now for this let's handle the cases where the paddock_id has a better if-statement that handle
 
-  if (paddock_id !== null && paddock_id !== undefined) {
-    db.prepare(
-      'UPDATE paddocks SET animal_count = animal_count + 1 WHERE id = ?'
-    ).run(paddock_id)
+    if (paddock_id !== null && paddock_id !== undefined) {
+      db.prepare(
+        'UPDATE paddocks SET animal_count = animal_count + 1 WHERE id = ?'
+      ).run(paddock_id)
+    }
+
+    const animal = db
+      .prepare('SELECT * FROM animals WHERE id = ?')
+      .get(result.lastInsertRowid)
+    res.json(animal)
+  
+  } catch (error) {
+    // Return API reponse when unique constraint is violated for tag_number
+    if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+      return res.status(400).json({
+        error: 'tag_number already exists'
+      })
+    }
+
+    throw error
   }
-
-  const animal = db
-    .prepare('SELECT * FROM animals WHERE id = ?')
-    .get(result.lastInsertRowid)
-  res.json(animal)
 })
 
 router.get('/:id', (req, res) => {
@@ -108,8 +121,6 @@ router.put('/:id', (req, res) => {
       ).run(updates.paddock_id)
     }
   }
-
-
 
   db.prepare(
     `
